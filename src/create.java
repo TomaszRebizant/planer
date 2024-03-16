98a7bb86b3d02dc7f1f35de6812f0e1768b81172
Cysiuu <cysiuu021@protonmail.com>
2024-03-16 22:02:08 +0100

    fixed create logic

--- src/create.java
+++ src/create.java
@@ -2,69 +2,89 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ComponentAdapter;
 import java.awt.event.ComponentEvent;
+import java.util.HashMap;
 
-public class create {
+public class create extends JFrame {
     private static JFrame frame;
-    private static JPanel choosemenu;
-    private static JPanel panel2;
+    private static JPanel chooseMenu;
+    private static JPanel secondPanel;
+    private static HashMap<String, JButton> buttons = new HashMap<>();
 
-    private static JButton button1;
+    public create() {
+        setFrame();
+        createAndShowGUI();
+    }
 
     public static void createAndShowGUI() {
-        setFrame();
-        setChoosemenu(frame.getWidth()/10, 50);
+
+        setChooseMenu(frame.getWidth()/10, frame.getWidth()/10);
         setPanel2(frame.getWidth(), frame.getHeight());
+        addWeekButtons();
 
-        button1 = new JButton("Poniedziałek");
-        choosemenu.add(button1);
-        buttonstyle(button1);
-        frame.setSize(700, 700);
-        frame.setVisible(true);
-        resize(button1, choosemenu, frame);
+
+        frame.add(chooseMenu, BorderLayout.WEST);
+        frame.add(secondPanel);
+
+        resize(chooseMenu, frame);
     }
-        public static void resize(JButton button1, JPanel choosemenu, JFrame frame){
-            button1.setPreferredSize(new Dimension(choosemenu.getWidth(), 100));
-            frame.addComponentListener(new ComponentAdapter() {
-                @Override
-                public void componentResized(ComponentEvent e) {
-                    choosemenu.setPreferredSize(new Dimension(frame.getWidth() /10, frame.getHeight()));
-                    button1.setPreferredSize(new Dimension(choosemenu.getWidth(), 50));
-                    frame.revalidate();
-                    frame.repaint();
+    public static void resize( JPanel choosemenu, JFrame frame){
+        frame.addComponentListener(new ComponentAdapter() {
+            @Override
+            public void componentResized(ComponentEvent e) {
+                choosemenu.setPreferredSize(new Dimension(frame.getWidth()/10, frame.getHeight()));
+                for (JButton button : buttons.values()) {
+                    button.setPreferredSize(new Dimension(75*frame.getWidth()/1000, 50*frame.getHeight()/1000));
                 }
-            });
-        }
-
-    public static void setChoosemenu(int width, int height) {
-        choosemenu = new JPanel();
-        choosemenu.setSize(width, height);
-        choosemenu.setLayout(new BorderLayout(0, 0));
-        frame.add(choosemenu, BorderLayout.WEST);
-        choosemenu.setBackground(new Color(235, 230, 206));
-
+                frame.revalidate();
+                frame.repaint();
+            }
+        });
     }
-public static void setFrame() {
-    frame = new JFrame();
-    frame.setTitle("Plan dnia");
-    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    frame.setSize(600, 600);
-    frame.setLayout(new BorderLayout(0, 0));
-}
- public static void setPanel2(int width, int height) {
-        panel2 = new JPanel();
-        panel2.setSize(width, height);
-        panel2.setLayout(new FlowLayout(FlowLayout.RIGHT, 0, 0));
-        frame.add(panel2, BorderLayout.CENTER);
-        panel2.setBackground(Color.BLUE);
+
+    public static void setChooseMenu(int width, int height) {
+        FlowLayout layout = new FlowLayout();
+        chooseMenu = new JPanel();
+        chooseMenu.setLayout(layout);
+        chooseMenu.setPreferredSize(new Dimension(width, height));
+        chooseMenu.setBackground(new Color(235, 230, 206));
     }
-    public static void buttonstyle(JButton button) {
-        button.setMinimumSize(new Dimension(50, 50));
+    public static void setFrame() {
+        frame = new JFrame("Plan dnia");
+        frame.setVisible(true);
+        frame.setSize(1366, 768);
+        frame.setLayout(new BorderLayout());
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+    public static void setPanel2(int width, int height) {
+        secondPanel = new JPanel();
+        secondPanel.setSize(width, height);
+        secondPanel.setBackground(Color.BLUE);
+    }
+    public static void addButton(JButton button) {
         button.setBackground(new Color(255, 208, 79));
-        button.setBorder(BorderFactory.createEmptyBorder());
-        button.setHorizontalAlignment(SwingConstants.LEFT);
+        button.setPreferredSize(new Dimension(75*frame.getWidth()/1000, 50*frame.getHeight()/1000));
         button.setFocusPainted(false);
-        button.setContentAreaFilled(false);
-        button.setOpaque(true);
+        button.setContentAreaFilled(true);
         button.setVisible(true);
+        button.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+        chooseMenu.add(button);
+        buttons.put(button.getText(), button);
+    }
+
+    public static void addWeekButtons(){
+        JButton monday = new JButton("Poniedziałek");
+        addButton(monday);
+        JButton tuesday = new JButton("Wtorek");
+        addButton(tuesday);
+        JButton thurdsay = new JButton("Środa");
+        addButton(thurdsay);
+        JButton wednesday = new JButton("Czwartek");
+        addButton(wednesday);
+        JButton friday = new JButton("Piątek");
+        addButton(friday);
+        JButton saturday = new JButton("Sobota");
+        addButton(saturday);
+        JButton sunday = new JButton("Niedziela");
+        addButton(sunday);
     }
 }
